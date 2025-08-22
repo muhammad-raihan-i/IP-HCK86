f61e1361629ccc94308db38a81252a7290a89dc2
muhammad-raihan-i <raihan.indrastoto@gmail.com>
2025-08-22 08:14:43 +0700

    chore: restart from zero due to copilot hallucination

--- client/src/helpers/http.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
-
-const http = axios.create({
-  baseURL: BASE_URL,
-});
-
-export default http;
